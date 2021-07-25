288e98816453f191e0594e40341a4ad636b2caee
hasanlikhayala <khasanli2021@gmail.com>
2021-07-25 17:37:58 +0400

    some changes

--- DesignExamples/View/Controllers/TabbarController/TabbarSetting.swift
+++ DesignExamples/View/Controllers/TabbarController/TabbarSetting.swift
@@ -3,10 +3,22 @@
 //  DesignExamples
 //
 //  Created by Samir Hasanli on 03.07.21.
+//Copyright (c) 2020 Ahmadalsofi <alsofiahmad@yahoo.com>
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
 //
 
 import Foundation
 import UIKit
+
 
 // Here you can customize the tab bar to meet your neededs
 public struct TabBarSetting {
